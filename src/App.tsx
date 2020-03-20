6ff917413bec1744677a2e94bc2ce16fa1eb040e
Morten Stulen <stulenmorten@gmail.com>
2020-03-20 23:11:39 +0100

    Add firestore connection

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,31 @@
+//@ts-nocheck
 import React from 'react'
 import './App.css'
+import { compose } from 'redux'
+import { firestoreConnect } from 'react-redux-firebase'
+import { connect } from 'react-redux'
+import moment from 'moment'
 
-function App() {
-  return <div>main</div>
+const App = ({ challenges }) => {
+  console.log(challenges)
+  return (
+    <div>
+      {challenges &&
+        challenges.map(challenge => {
+          return (
+            <div key={challenge.id}>
+              <p>{challenge.name}</p>
+              <p>{moment(challenge.date.seconds * 1000).format('l')}</p>
+              <p>{challenge.comment}</p>
+            </div>
+          )
+        })}
+    </div>
+  )
 }
-
-export default App
+export default compose(
+  firestoreConnect(() => ['challenges']),
+  connect(state => ({
+    challenges: state.firestore.ordered.challenges
+  }))
+)(App)
--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,72 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import * as serviceWorker from './serviceWorker'
-import { BrowserRouter, useLocation } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import Routes from './navigation/Routes'
+import { Provider } from 'react-redux'
 import Navbar from './components/navbar/Navbar'
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/firestore'
+import 'firebase/analytics'
+
+import { createStore, combineReducers } from 'redux'
+import {
+  ReactReduxFirebaseProvider,
+  firebaseReducer
+} from 'react-redux-firebase'
+import { createFirestoreInstance, firestoreReducer } from 'redux-firestore'
+
+var firebaseConfig = {
+  apiKey: 'AIzaSyBK18bmdoHwzc4TVdp6pM8qdDI4C6_mwxk',
+  authDomain: 'challenges-scoreboard.firebaseapp.com',
+  databaseURL: 'https://challenges-scoreboard.firebaseio.com',
+  projectId: 'challenges-scoreboard',
+  storageBucket: 'challenges-scoreboard.appspot.com',
+  messagingSenderId: '718152674889',
+  appId: '1:718152674889:web:30478c643195eac869d998',
+  measurementId: 'G-HLT387EGY8'
+}
+
+// react-redux-firebase config
+const rrfConfig = {
+  userProfile: 'users',
+  useFirestoreForProfile: true
+}
+
+// Initialize firebase instance
+firebase.initializeApp(firebaseConfig)
+
+// Initialize other services on firebase instance
+firebase.firestore()
+firebase.analytics()
+
+// Add firebase to reducers
+const rootReducer = combineReducers({
+  firebase: firebaseReducer,
+  firestore: firestoreReducer
+})
+
+// Create store with reducers and initial state
+const initialState = {}
+const store = createStore(rootReducer, initialState)
+
+const rrfProps = {
+  firebase,
+  config: rrfConfig,
+  dispatch: store.dispatch,
+  createFirestoreInstance
+}
+
 ReactDOM.render(
-  <BrowserRouter>
-    <Navbar />
-    <Routes />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <ReactReduxFirebaseProvider {...rrfProps}>
+      <BrowserRouter>
+        <Navbar />
+        <Routes />
+      </BrowserRouter>
+    </ReactReduxFirebaseProvider>
+  </Provider>,
 
   document.getElementById('root')
 )
