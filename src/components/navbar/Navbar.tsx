ebb2c065724bc38c68dceaa41da0fea1d3d1ec6d
Morten Stulen <stulenmorten@gmail.com>
2020-03-21 00:22:28 +0100

    Add navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,19 +1,21 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import './Navbar.css'
+import icon from '../../assets/icons/logo.png'
 interface NavbarProps {}
 
 const Navbar = (props: NavbarProps) => {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Hjem</Link>
-        </li>
-        <li>
-          <Link to="/login">Login</Link>
-        </li>
-      </ul>
-    </nav>
+    <div className="navbar">
+      <img src={icon} alt="" />
+      <a href="">Registrer</a>
+      <a href="">Leaderboard</a>
+      <a href="">Stats</a>
+      <a href="">Siste spill</a>
+      <a id="hjem" href="">
+        Hjem
+      </a>
+    </div>
   )
 }
 
