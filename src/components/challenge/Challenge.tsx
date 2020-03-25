885a6f88cc65735f8f4db8be3d2d75cc25b15b89
Morten Stulen <stulenmorten@gmail.com>
2020-03-25 15:27:02 +0100

    Challenge css

--- src/components/challenge/Challenge.tsx
+++ src/components/challenge/Challenge.tsx
@@ -1,6 +1,8 @@
+//@ts-nocheck
 import React from 'react'
 import { ChallengeObject } from '../../shared/Types'
 import moment from 'moment'
+import './Challenge.css'
 
 interface ChallengeProps {
   challenge: ChallengeObject
@@ -9,9 +11,20 @@ interface ChallengeProps {
 const Challenge = ({ challenge }: ChallengeProps) => {
   return (
     <div key={challenge.id} className='challenge'>
-      <p>{challenge.name}</p>
-      <p>{moment(challenge.date.seconds * 1000).format('l')}</p>
-      <p>{challenge.comment}</p>
+      <div className='text-container'>
+        <p>
+          {challenge.name + ' '}
+          <span className='date'>
+            {moment(challenge.date.seconds * 1000).format('l')}
+          </span>
+        </p>
+        <p>{challenge.comment}</p>
+      </div>
+      <div className='tag-container'>
+        <p>Sport</p>
+        <p>Sport</p>
+        <p>Sport</p>
+      </div>
     </div>
   )
 }
